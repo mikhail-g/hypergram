0c6be8e43d435366e104677fd4875af2e3df0965
Misha Hariachyi <dev.Hariachyi.Misha@dev.bwater.com>
2021-10-20 06:23:51 -0400

    Finished phase 2 of the project

--- Hypergram/task/test/test.js
+++ Hypergram/task/test/test.js
@@ -1,8 +1,13 @@
 const puppeteer = require('puppeteer');
+const pixels = require('image-pixels')
 const path = require('path');
-const pagePath = 'file://' + path.resolve(__dirname, '../src/index.html');
+
 const hs = require('hs-test-web');
+const {onPage} = require('hs-test-web');
 const sleep = (ms) => new Promise(res => setTimeout(res, ms));
+
+const pagePath = 'file://' + path.resolve(__dirname, '../src/index.html');
+const filePath = path.resolve(__dirname, '../test/testImage.png');
 
 async function stageTest() {
 
@@ -10,7 +15,8 @@ async function stageTest() {
         headless: false,
         defaultViewport: null,
         args: ['--start-maximized', '--disable-infobar'],
-        ignoreDefaultArgs: ['--enable-automation']
+        ignoreDefaultArgs: ['--enable-automation'],
+        devtools: true
     });
 
     const page = await browser.newPage();
@@ -20,8 +26,8 @@ async function stageTest() {
 
     await sleep(1000);
 
-    let result = await hs.testPage(page,
-        () => {
+    let result = await hs.test(
+        onPage(page, () => {
             const canvas = document.getElementsByTagName("canvas");
             if (canvas.length !== 1) {
                 return hs.wrong("There is should be 1 canvas element in the page!")
@@ -35,7 +41,7 @@ async function stageTest() {
             const brightnessSlider = document.querySelector("input[type='range']#brightness");
             if (brightnessSlider === null) {
                 return hs.wrong("Can't slider for brightness parameter. " +
-                    "There is should be an 'input' tag with type 'range' and with #brightness id!")
+                    "There is should be an 'input' tag with type 'range' and with #brigtness id!")
             }
 
             const contrastSlider = document.querySelector("input[type='range']#contrast");
@@ -53,6 +59,35 @@ async function stageTest() {
             const saveButton = document.querySelector("button#save-button");
             if (saveButton === null) {
                 return hs.wrong("Can't find a button with #save-button id!")
+            }
+
+            return hs.correct()
+        }),
+        async () => {
+            const uploadButton = await page.$("input[type='file']#file-input");
+            await uploadButton.uploadFile(filePath);
+            await uploadButton.evaluate(upload => upload.dispatchEvent(new Event('change', {bubbles: true})));
+            await sleep(500)
+
+            const userPixels = await page.evaluate(() => {
+                const canvas = document.getElementsByTagName("canvas")[0];
+                if (canvas.width !== 30 || canvas.height !== 30) {
+                    return hs.wrong("After uploading an image into canvas it has wrong size!")
+                }
+                const ctx = canvas.getContext("2d");
+                return ctx.getImageData(0, 0, canvas.width, canvas.height).data;
+            });
+
+            const {data} = await pixels(filePath)
+
+            if (data.length !== Object.keys(userPixels).length) {
+                return hs.wrong("Wrong number ox pixels on the canvas!")
+            }
+
+            for (let i = 0; i < data.length; i++) {
+                if (data[i] !== userPixels[i]) {
+                    return hs.wrong("Looks like some of the pixels have wrong RGB value!")
+                }
             }
 
             return hs.correct()
