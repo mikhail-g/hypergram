af6bf48ecb99b7e716c34f9640392ae5e4de1e86
Misha Hariachyi <dev.Hariachyi.Misha@dev.bwater.com>
2021-10-22 06:11:13 -0400

    Finished stage 4

--- Hypergram/task/src/listeners.js
+++ Hypergram/task/src/listeners.js
@@ -2,25 +2,25 @@ let originalPixels;
 const DEFAULT_BRIGHTNESS = 0;
 const DEFAULT_CONTRAST = 0;
 const DEFAULT_TRANSPARENT = 1;
-const RANGE_EVENT_TYPE = 'input';
+const RANGE_EVENT_TYPE = 'change';
 
 let brightness;
 let contrast;
 let transparent;
 
-const getCanvas = () => document.getElementById("canvas");
+const getCanvas = () => document.getElementById('canvas');
 const getCtx = () => getCanvas().getContext('2d');
 
 const drawImageOnload = image => (_) => {
     const canvas = getCanvas();
-    const ctx = canvas.getContext('2d');
+    const ctx = getCtx();
     canvas.width = image.width;
     canvas.height = image.height;
     ctx.drawImage(image, 0, 0);
     originalPixels = ctx.getImageData(0, 0, canvas.width, canvas.height).data;
 };
 
-const fileInput = document.getElementById("file-input");
+const fileInput = document.getElementById('file-input');
 fileInput.addEventListener('change', (ev) => {
     if (ev.target.files) {
         const file = ev.target.files[0];
@@ -37,7 +37,7 @@ fileInput.addEventListener('change', (ev) => {
 
 const processPixels = (initialPixels, colorFun, alphaFun) => {
     const canvas = getCanvas();
-    const ctx = canvas.getContext('2d');
+    const ctx = getCtx();
     const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
     const pixels = imageData.data;
     pixels.set(initialPixels);
@@ -53,15 +53,12 @@ const processPixels = (initialPixels, colorFun, alphaFun) => {
         pixels[redIndex + 2] = colorFun(BLUE);
         pixels[redIndex + 3] = alphaFun(ALPHA);
     }
-    getCtx().putImageData(imageData, 0, 0);
+    ctx.putImageData(imageData, 0, 0);
 };
 
 
 const applyAdjustments = (Brightness, Contrast, Transparent) => {
     if (originalPixels !== undefined) {
-        console.log(`Brightness:\t ${Brightness}`);
-        console.log(`Contrast:\t ${Contrast}`);
-        console.log(`Transparent:\t ${Transparent}`);
         const Truncate = (number) => number < 0 ? 0 : (number > 255 ? 255 : number);
         const Factor = 259 * (255 + Contrast) / (255 * (259 - Contrast));
         const adjustContrast = (color) => (Factor * (color - 128) + 128);
@@ -74,19 +71,19 @@ const applyAdjustments = (Brightness, Contrast, Transparent) => {
 
 const applySavedAdjustments = () => applyAdjustments(brightness, contrast, transparent);
 
-const brightnessRange = document.getElementById("brightness");
+const brightnessRange = document.getElementById('brightness');
 brightnessRange.addEventListener(RANGE_EVENT_TYPE, (ev) => {
     brightness = parseInt(ev.target.value);
     applySavedAdjustments();
 });
 
-const contrastRange = document.getElementById("contrast");
+const contrastRange = document.getElementById('contrast');
 contrastRange.addEventListener(RANGE_EVENT_TYPE, (ev) => {
     contrast = parseInt(ev.target.value);
     applySavedAdjustments();
 });
 
-const transparentRange = document.getElementById("transparent");
+const transparentRange = document.getElementById('transparent');
 transparentRange.addEventListener(RANGE_EVENT_TYPE, (ev) => {
     transparent = parseFloat(ev.target.value);
     applySavedAdjustments();
@@ -105,5 +102,17 @@ getCanvas().addEventListener('mousedown', (_ev) => applyAdjustments(DEFAULT_BRIG
 
 getCanvas().addEventListener('mouseup', (_ev) => applyAdjustments(brightness, contrast, transparent));
 
-const resetButton = document.getElementById("reset-button");
+const resetButton = document.getElementById('reset-button');
 resetButton.addEventListener('click', (_ev) => resetAdjustments());
+
+const caveButton = document.getElementById('save-button');
+caveButton.addEventListener('click', (_ev) => {
+    const imageData = getCanvas().toDataURL();
+    const tmpLink = document.createElement('a');
+    tmpLink.download = 'result.png';
+    tmpLink.href = imageData;
+
+    document.body.appendChild( tmpLink );
+    tmpLink.click();
+    document.body.removeChild( tmpLink );
+});
